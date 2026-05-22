fcb760ece869171f229bfd3be1dfbdbb19ec2078
Youssef <romdhaniyoussef97@gmail.com>
2026-05-22 17:02:06 +0200

    Created a class for book based on Base

---
+++ backend/models/book.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Text
+from database import Base 
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    author = Column(String, nullable=False)
+    year = Column(Integer, nullable=False)
+    genre = Column(String, nullable=False)
+    description = Column(Text, nullable=True)
+
